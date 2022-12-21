685430bd2a713dddcc63d11785b9298c9f0ffd35
Mikhail <mehan@list.ru>
2022-12-21 21:44:35 +0300

    gui is done

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import view
 
-if __name__ == '__main__':
-    view.app_window()
+if __name__ == "__main__":
+    import sys
+    app = view.QtWidgets.QApplication(sys.argv)
+    MainWindow = view.QtWidgets.QMainWindow()
+    ui = view.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
